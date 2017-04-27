e90397fed3e34c423c05905f6d4f926f7cb06478
stevengatsios <stevengatsios@gmail.com>
2017-04-27 19:52:36 +0300

    Update

---
+++ documents/Topic.py
@@ -0,0 +1,48 @@
+﻿from flask_mongokit import Document
+from mongokit import CustomType, IS
+from datetime import datetime
+from pprint import pprint
+
+# Python 2
+class CustomDate(CustomType):
+    mongo_type = unicode
+    python_type = datetime # optional, just for more validation
+    init_type = None # optional, fill the first empty value
+
+    def to_bson(self, value):
+        """convert type to a mongodb type"""
+        #pprint(value)
+        return unicode(value.isoformat("T") + "Z")
+        # return datetime.strptime(value, '%m-%d-%Y')
+
+    def to_python(self, value):
+        """convert type to a python object"""
+        if value is not None:
+           # pprint("to python : " + value)
+           return datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%fZ')
+           
+           # return unicode(datetime.strftime(value,'%m-%d-%Y'))
+           
+           
+           
+
+    def validate(self, value, path):
+        """OPTIONAL : useful to add a validation layer"""
+        if value is not None:
+            pass # ... do something here
+
+
+class Topic(Document):
+    __collection__ = 'tasks'
+    structure = {
+        'name' : unicode,
+        'description' : unicode,
+        'date' : CustomDate(),
+        'weight' : float,
+        'questionOrder' : IS(u"hits",u"weight")
+    }
+    required_fields = ['name', 'description','weight','questionOrder']
+    # default_values = {'date': datetime.utcnow}
+    use_dot_notation = True
+
+
--- run.py
+++ run.py
@@ -1,9 +1,8 @@
-# from flask_pymongo import PyMongo
+﻿# from flask_pymongo import PyMongo
 
 from flask import Flask, jsonify, request, Response
 
 from flask_mongokit import MongoKit, Document
-from mongokit import CustomType, IS
 import json
 from bson import ObjectId, json_util
 from bson.json_util import dumps
@@ -12,46 +11,18 @@ from flask_cors import CORS, cross_origin
 
 from pprint import pprint
 
-# Python 2
-class CustomDate(CustomType):
-    mongo_type = datetime
-    python_type = unicode # optional, just for more validation
-    init_type = None # optional, fill the first empty value
-
-    def to_bson(self, value):
-        """convert type to a mongodb type"""
-        return datetime.strptime(value, '%m-%d-%Y')
-
-    def to_python(self, value):
-        """convert type to a python object"""
-        if value is not None:
-           return unicode(datetime.strftime(value,'%m-%d-%Y'))
-           
-
-    def validate(self, value, path):
-        """OPTIONAL : useful to add a validation layer"""
-        if value is not None:
-            pass # ... do something here
-
-class Topic(Document):
-    __collection__ = 'tasks'
-    structure = {
-        'name' : unicode,
-        'description' : unicode,
-        'date' : CustomDate(),
-        'weight' : float,
-        'questionOrder' : IS(u"hits",u"weight")
-    }
-    required_fields = ['name', 'description','weight','questionOrder']
-    default_values = {'date': datetime.utcnow}
-    use_dot_notation = True
-
-    def make_id(self):
-    	pprint(self)
-    	self['id'] = str(self._id)
-    	return self
+from documents.Topic import Topic
 
 
+
+class ComplexEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, ObjectId):
+            return str(obj)
+        elif hasattr(obj, 'isoformat'):
+            return obj.isoformat()
+        # Let the base class default method raise the TypeError
+        return json.JSONEncoder.default(self, obj)
 
 app = Flask(__name__)
 CORS(app)
@@ -62,26 +33,35 @@ mongo.register([Topic])
 
 @app.route('/topic', methods=['GET'])
 def get_all_topics():
-  topic = mongo.Topic
-  output = [t.make_id() for t in topic.find()]
-  return Response(dumps(output),mimetype='application/json')
+    topic = mongo.Topic
+    output = [t for t in topic.find()]
+    # return Response(dumps(output),mimetype='application/json')
+    return Response(json.dumps(output,cls=ComplexEncoder),mimetype='application/json')
+  
 
 @app.route('/topic', methods=['DELETE'])
 def delete_all_topics():
-  topic = mongo.Topic
-  mongo.Topic.collection.remove()
-  return Response(dumps({"status" : "OK"}),mimetype='application/json')
+    topic = mongo.Topic
+    mongo.Topic.collection.remove()
+    return Response(dumps({"status" : "OK"}),mimetype='application/json')
+
+@app.route('/topic/<string:topic_id>', methods=['DELETE'])
+def delete_topic(topic_id):
+    topic = mongo.Topic
+    topic = mongo.Topic.find_one({'_id': ObjectId(topic_id) })
+    topic.delete()
+    return Response(dumps({"status" : "OK"}),mimetype='application/json')
 
 @app.route('/topic/<string:topic_id>', methods=['GET'])
 def get_topic(topic_id):
     topic = mongo.Topic.find_one({'_id': ObjectId(topic_id) })
-    return Response(dumps(topic),mimetype='application/json')
+    return Response(json.dumps(topic,cls=ComplexEncoder),mimetype='application/json')
 
 @app.route('/topic', methods=['POST'])
 def add_topic():
-  topic = mongo.Topic.from_json(str(request.data))
-  topic.save()
-  return Response(dumps(topic),mimetype='application/json')
+    topic = mongo.Topic.from_json(str(request.data))
+    topic.save()
+    return Response(json.dumps(topic,cls=ComplexEncoder),mimetype='application/json')
 
 
 
